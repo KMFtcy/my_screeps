8f8dddb468827a9820c3f82009030ac0246056c6
kmftcy <kmftangchaoyang@gmail.com>
2022-04-23 12:14:54 +0800

    now harvester only send energy to container and spawn

--- role.harvester.js
+++ role.harvester.js
@@ -1,34 +1,34 @@
 var roleHarvester = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
-	    if(creep.store.getFreeCapacity() > 0) {
+    run: function (creep) {
+        if (creep.store.getFreeCapacity() > 0) {
             var source = creep.pos.findClosestByPath(FIND_SOURCES)
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } });
             }
         }
         else {
             var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_CONTAINER ||
-                                structure.structureType == STRUCTURE_TOWER) && 
-                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                filter: (structure) => {
+                    return (
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_CONTAINER
+                    ) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
-            }else{
+            });
+            if (targets.length > 0) {
+                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
+                }
+            } else {
                 // if can't find a place to put energy, just find a place to stay
                 var spawns_target = creep.room.find(FIND_MY_SPAWNS)
                 creep.moveTo(spawns_target[0])
             }
         }
-	}
+    }
 };
 
 module.exports = roleHarvester;
